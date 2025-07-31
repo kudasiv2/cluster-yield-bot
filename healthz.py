1829bd6a49660ca350e6cee7d1ceb1a59815915c
kudasiv2 <fadliahlam919@gmail.com>
2025-07-31 23:19:04 +0700

    Update healthz.py

--- healthz.py
+++ healthz.py
@@ -8,4 +8,4 @@ def health_check():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 3000)))
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", "3000")))
