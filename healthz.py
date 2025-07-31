64918c7af76cb16988044677e0a25c758c67a6ea
kudasiv2 <fadliahlam919@gmail.com>
2025-07-31 19:41:23 +0700

    healthz.py

---
+++ healthz.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "OK", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 3000)))
